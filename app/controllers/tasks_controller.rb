b99563404a9543b5803a2e21df1abd2652e141ba
danielle misaine <daniellemisaine@gmail.com>
2022-03-08 17:05:43 -0400

    exo en cours

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,12 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  def new
+    @task = Task.new
+  end
+  def show
+    @task = Task.find(params[:id])
+  end
+
+end
